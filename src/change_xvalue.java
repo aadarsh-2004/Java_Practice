e63fcb2357366f7fb9de7262349cf18194b9a023
aadarsh-2004 <2020aadarshsoni@gmail>
2024-02-29 18:35:46 +0530

    added

--- src/change_xvalue.java
+++ src/change_xvalue.java
@@ -1,5 +1,12 @@
+import  java.util.Arrays;
 public class change_xvalue {
     public static void main(String[] args) {
-        System.out.println("helllo");
+        int[] arr ={1,2,3,4,5};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
     }
+    static void change(int[] nums){
+        nums[0]=500;
+    }
+
 }
